c308e47bc45036b62a09e06c5d48f5007520091a
zzaeun <leeje57575@gmail.com>
2025-08-21 11:25:44 +0900

    나머지 연산 가능, 0으로 나눌 경우 발생하는 오류 Optional 사용해 해결

--- Contents.swift
+++ Contents.swift
@@ -3,32 +3,41 @@ class Calculator {
 
     var num1: Int
     var num2: Int
-
+    
     init(num1: Int, num2: Int) {
         self.num1 = num1
         self.num2 = num2
     }
-
+    
     func add() -> Int {
         return num1 + num2
     }
-
+    
     func sub() -> Int {
         return num1 - num2
     }
-
-    func div() -> Int {
-        return num1 / num2
+    
+    // 나머지 연산
+    func div1() -> Int? {
+        if num2 == 0 { return nil }
+        return num1 % num2
     }
 
+    // 몫 연산
+    func div2() -> Int? {
+        if num2 == 0 { return nil }
+        return num1 / num2
+    }
+    
     func mul() -> Int {
         return num1 * num2
     }
 }
 
-let calculator = Calculator(num1: 3, num2: 8)
+let calculator = Calculator(num1: 6, num2: 3)
+
 print(calculator.add())
 print(calculator.sub())
-print(calculator.div())
+print(calculator.div1())
+print(calculator.div2())
 print(calculator.mul())
-
