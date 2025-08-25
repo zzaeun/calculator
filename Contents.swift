a97f6c05cbe994aeba3ee5a35cf5fdf8659e6041
zzaeun <leeje57575@gmail.com>
2025-08-25 16:26:26 +0900

    보완) Lv1 퀵헬프 추가, 나눗셈 Double로 반환

--- Contents.swift
+++ Contents.swift
@@ -1,74 +1,37 @@
+
+/// - Parameters:
+/// - num1: 연산자의 왼쪽에 존재하는 수
+/// - num2: 연산자의 오른쪽에 존재하는 수
+///
 class Calculator {
-    let addOperation = AddOperation()
-    let substractOperation = SubstractOperation()
-    let multiplyOperation = MultiplyOperation()
-    let remainderOperation = DivideOperation1()
-    let quotientOperation = DivideOperation2()
     
-    func add(_ num1: Int, _ num2: Int) -> Int {
-        return addOperation.add(num1, num2)
-    }
+        var num1: Int
+        var num2: Int
     
-    func sub(_ num1: Int, _ num2: Int) -> Int {
-        return substractOperation.sub(num1, num2)
-    }
+        init(num1: Int, num2: Int) {
+            self.num1 = num1
+            self.num2 = num2
+        }
     
-    func mul(_ num1: Int, _ num2: Int) -> Int {
-        return multiplyOperation.mul(num1, num2)
-    }
+        func add() -> Int {
+            return num1 + num2
+        }
     
-    func remainder(_ num1: Int, _ num2: Int) -> Int? {
-        return remainderOperation.remainder(num1, num2)
-    }
+        func sub() -> Int {
+            return num1 - num2
+        }
     
-    func quotient(_ num1: Int, _ num2: Int) -> Int? {
-        return quotientOperation.quotient(num1, num2)
-    }
-}
-
-class AddOperation {
-    func add(_ num1: Int, _ num2: Int) -> Int {
-        return num1 + num2
-    }
-}
-
-class SubstractOperation {
-    func sub(_ num1: Int, _ num2: Int) -> Int {
-        return num1 - num2
-    }
-}
-
-class MultiplyOperation {
-    func mul(_ num1: Int, _ num2: Int) -> Int {
-        return num1 * num2
-    }
-}
-
-class DivideOperation1 {
-    // 나머지
-    func remainder(_ num1: Int, _ num2: Int) -> Int? {
-        if let number = (num2 == 0 ? nil : num1 % num2) {
-            return number
-        } else {
-            return nil
+        func div() -> Double {
+            return Double(num1) / Double(num2)
+        }
+    
+        func mul() -> Int {
+            return num1 * num2
         }
     }
-}
-
-class DivideOperation2 {
-    // 몫
-    func quotient(_ num1: Int, _ num2: Int) -> Int? {
-        guard let number = (num2 == 0 ? nil : num1 / num2) else {
-            return nil
-        }
-            return number
-    }
     
-}
-
-let cal = Calculator()
-print(cal.add(5, 3))
-print(cal.sub(5, 3))
-print(cal.mul(5, 3))
-print(cal.remainder(6, 2))
-print(cal.quotient(8, 0))
+    let calculator = Calculator(num1: 3, num2: 8)
+    print(calculator.add())
+    print(calculator.sub())
+    print(calculator.div())
+    print(calculator.mul())
