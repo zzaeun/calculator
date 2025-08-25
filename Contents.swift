aa54f640dfa996cec5f0777071b74d4528fbabd6
zzaeun <leeje57575@gmail.com>
2025-08-25 19:17:08 +0900

    보완) Lv2 나머지 연산 Double로 가능하게, 몫 연산 Double로 가능하게하고 결과는 소수점 둘째자리까지

--- Contents.swift
+++ Contents.swift
@@ -1,37 +1,51 @@
+import Foundation
 
-/// - Parameters:
-/// - num1: 연산자의 왼쪽에 존재하는 수
-/// - num2: 연산자의 오른쪽에 존재하는 수
-///
 class Calculator {
     
         var num1: Int
         var num2: Int
-    
+        
         init(num1: Int, num2: Int) {
             self.num1 = num1
             self.num2 = num2
         }
-    
+        
         func add() -> Int {
             return num1 + num2
         }
-    
+        
         func sub() -> Int {
             return num1 - num2
         }
-    
-        func div() -> Double {
-            return Double(num1) / Double(num2)
-        }
-    
+        
+    // 나머지 연산
+          func div1() {
+              if let number = (num2 == 0 ? nil : Double(num1).truncatingRemainder(dividingBy: Double(num2))) {
+                  print("\(number)")
+              } else {
+                  print("정의할 수 없음")
+              }
+          }
+      
+          // 몫 연산
+          func div2() {
+              guard let number = (num2 == 0 ? nil : Double(num1) / Double(num2)) else {
+                  print("정의할 수 없음")
+                  return
+              }
+              let rounded = round(number * 100) / 100
+              print(rounded)
+          }
+        
         func mul() -> Int {
             return num1 * num2
         }
     }
     
-    let calculator = Calculator(num1: 3, num2: 8)
+    let calculator = Calculator(num1: 9, num2: 3)
+    
     print(calculator.add())
     print(calculator.sub())
-    print(calculator.div())
+    calculator.div1()
+    calculator.div2()
     print(calculator.mul())
