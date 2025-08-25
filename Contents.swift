032db58fb9c5b05f2889d17a071e5e5efb6d32f1
zzaeun <leeje57575@gmail.com>
2025-08-25 19:50:01 +0900

    Lv3 몫, 나머지 연산 Double로 반환되게

--- Contents.swift
+++ Contents.swift
@@ -1,51 +1,95 @@
 import Foundation
 
-class Calculator {
-    
-        var num1: Int
-        var num2: Int
-        
-        init(num1: Int, num2: Int) {
-            self.num1 = num1
-            self.num2 = num2
-        }
-        
-        func add() -> Int {
-            return num1 + num2
-        }
-        
-        func sub() -> Int {
-            return num1 - num2
-        }
-        
-    // 나머지 연산
-          func div1() {
-              if let number = (num2 == 0 ? nil : Double(num1).truncatingRemainder(dividingBy: Double(num2))) {
-                  print("\(number)")
-              } else {
-                  print("정의할 수 없음")
-              }
-          }
-      
-          // 몫 연산
-          func div2() {
-              guard let number = (num2 == 0 ? nil : Double(num1) / Double(num2)) else {
-                  print("정의할 수 없음")
-                  return
-              }
-              let rounded = round(number * 100) / 100
-              print(rounded)
-          }
-        
-        func mul() -> Int {
-            return num1 * num2
+class AddOperation {
+    func add(_ num1: Int, _ num2: Int) -> Int {
+        return num1 + num2
+    }
+}
+
+class SubstractOperation {
+    func sub(_ num1: Int, _ num2: Int) -> Int {
+        return num1 - num2
+    }
+}
+
+class MultiplyOperation {
+    func mul(_ num1: Int, _ num2: Int) -> Int {
+        return num1 * num2
+    }
+}
+
+class DivideOperation1 {
+    // 나머지
+    func remainder(_ num1: Int, _ num2: Int) -> Double? {
+        if let number = (num2 == 0 ? nil : Double(num1).truncatingRemainder(dividingBy: Double(num2))) {
+            return number
+        } else {
+            return nil
         }
     }
+}
+
+class DivideOperation2 {
+    // 몫
+    func quotient(_ num1: Int, _ num2: Int) -> Double? {
+        guard let number = (num2 == 0 ? nil : Double(num1) / Double(num2)) else {
+            return nil
+        }
+        let rounded = round(number * 100) / 100
+        return rounded
+    }
+}
+
+enum OperatorType {
+    case add
+    case sub
+    case mul
+    case remainder
+    case quotient
+}
+
+
+/// - Parameters:
+/// - num1: 연산자의 왼쪽에 존재하는 수
+/// - num2: 연산자의 오른쪽에 존재하는 수
+
+class Calculator {
+    let addOperation = AddOperation()
+    let substractOperation = SubstractOperation()
+    let multiplyOperation = MultiplyOperation()
+    let remainderOperation = DivideOperation1()
+    let quotientOperation = DivideOperation2()
     
-    let calculator = Calculator(num1: 9, num2: 3)
+    func add(_ num1: Int, _ num2: Int) -> Int {
+        return addOperation.add(num1, num2)
+    }
     
-    print(calculator.add())
-    print(calculator.sub())
-    calculator.div1()
-    calculator.div2()
-    print(calculator.mul())
+    func sub(_ num1: Int, _ num2: Int) -> Int {
+        return substractOperation.sub(num1, num2)
+    }
+    
+    func mul(_ num1: Int, _ num2: Int) -> Int {
+        return multiplyOperation.mul(num1, num2)
+    }
+    
+    func remainder(_ num1: Int, _ num2: Int) -> Double? {
+        return remainderOperation.remainder(num1, num2)
+    }
+    
+    func quotient(_ num1: Int, _ num2: Int) -> Double? {
+        return quotientOperation.quotient(num1, num2)
+    }
+}
+
+enum CalculationResult {
+    case int(Int)
+    case double(Double)
+}
+
+let cal = Calculator()
+
+print(cal.add(5, 3))
+print(cal.sub(5, 3))
+print(cal.mul(5, 3))
+print(cal.remainder(5, 2))
+print(cal.quotient(8, 3))
